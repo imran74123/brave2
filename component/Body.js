a85a35d1a7263ea4992e6ceabddf394b00596cc5
imran74123 <imranouhmoudou@gmail.com>
2023-11-06 01:37:39 +0100

    first-try

---
+++ component/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Body = () => {
+  return (
+    <main>
+      <p>This is the body of my Next.js website.</p>
+    </main>
+  );
+};
+export default Body;
---
+++ component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2023 My Next.js Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
